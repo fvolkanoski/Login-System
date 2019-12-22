f61685a53bdcd79ccb2ea2d12653b7c91bdd1908
fvolkanoski <f.volkanoski@gmail.com>
2019-12-23 00:55:16 +0100

    Added common style file, started change of profile page, added favicon.

--- src/welcome.php
+++ src/welcome.php
@@ -1,19 +1,22 @@
 <?php include_once "backend/welcome_bk.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Login System
-    </title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="style/welcome.css?v=<?=time();?>">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
 
+	<head>
+		<title>Shelf Social</title>
+		<link rel="shortcut icon" type="image/png" href="img/logo.png"/>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="style/common.css?v=<?=time();?>">
+		<link rel="stylesheet" type="text/css" href="style/welcome.css?v=<?=time();?>">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	</head>
+
+<body>
  <!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- NAVBAR -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom">
         <a class="navbar-brand navbar-custom-brand" href="index.php">Shelf Social</a>
@@ -174,6 +177,5 @@
 		</center>
 	</div>
 <!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- /RIGHT SIDEBAR -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- -->
-
-  </body>
+</body>
 </html>
